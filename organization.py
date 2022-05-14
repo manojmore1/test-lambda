ad2bfb726ce277e20262956f2450f83acaca0842
Manoj D More <manoj.contactpoint@gmail.com>
2022-05-14 14:08:34 +0530

    organization lambda

---
+++ organization.py
@@ -0,0 +1,32 @@
+import pymongo
+import json
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    print('PyCharm')
+
+    client = pymongo.MongoClient(
+        "mongodb+srv://admin:admin@mongomanojcluster.bsqoa.mongodb.net/pharmadb?retryWrites=true&w=majority")
+    db = client.pharmadb
+
+    mycollection = db['organizations']
+
+    rec = {
+        'orgName': 'Manoj xxx',
+        'gstNo': '0000',
+        'licenseNo': 'l5556',
+        'phoneNo': '234234234'
+    }
+
+    rec = db.organizations.insert_one(rec)
+    print(rec.inserted_id)
+
+    if rec is None:
+        print("result is None")
+
+    print('Data inserted to MongoDB')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
